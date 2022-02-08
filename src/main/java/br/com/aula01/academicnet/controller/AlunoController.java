d3fc9ac335dcb228b9fc24b02e6040ea6356233c
aparecido nepomuceno <isacnepomuceno.np@gmail.com>
2022-02-08 16:50:05 -0300

    alterar, deletar e ver aluno

--- src/main/java/br/com/aula01/academicnet/controller/AlunoController.java
+++ src/main/java/br/com/aula01/academicnet/controller/AlunoController.java
@@ -2,10 +2,14 @@ package br.com.aula01.academicnet.controller;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +32,21 @@ public class AlunoController {
 	@PostMapping
 	public void incluir(@RequestBody Aluno aluno) {
 		alunoRepositorio.save(aluno);
+	}
+	
+	@PutMapping
+	public void alterar(@RequestBody Aluno aluno) {
+		alunoRepositorio.save(aluno);
+	}
+	
+	@DeleteMapping("/{matricula}")
+	public void deletar(@PathVariable Long matricula) {
+		alunoRepositorio.deleteById(matricula);
+	}
+	
+	@GetMapping("/{matricula}")
+	public Optional<Aluno> ler(@PathVariable Long matricula) {
+		return alunoRepositorio.findById(matricula);
 	}
 }
 
